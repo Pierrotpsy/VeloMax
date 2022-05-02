a2467615d32b6c66526489af9135293e6e4877fc
Pierrotpsy <67154936+Pierrotpsy@users.noreply.github.com>
2022-05-02 13:27:51 +0200

    Add files via upload

---
+++ VeloMax - populate.sql
@@ -0,0 +1,25 @@
+
+
+#table Bicyclette
+
+#table Pièce
+
+#table Fournisseur
+
+#table Entreprise
+
+#table Particulier
+
+#table Commande
+
+#table Fidelio
+
+#table Assembler
+
+#table Fourni
+
+#table Contient_B
+
+#table Contient_P
+
+#table Adhere
